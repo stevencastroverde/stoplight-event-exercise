193e43e295cd44d1784d81ca25b123f6283ea261
Steven Castroverde <steven.castroverde@gmail.com>
2016-10-31 23:11:05 -0600

    was able to make lights change color

--- script.js
+++ script.js
@@ -3,8 +3,26 @@
 
   When I click on the 'stop' button,
     the top light should turn red.
+
+
+
   When I click on the 'slow' button
     the middle light should turn orange.
   When I click on the 'go' button
     the bottom light should turn green.
 */
+//Stop light
+	document.getElementById('stopButton').addEventListener('click', function(){
+		document.getElementById('stopLight').style.background = 'red';
+	})
+
+//Slow Button
+	document.getElementById('slowButton').addEventListener('click', function (){
+		document.getElementById('slowLight').style.background = 'orange';
+
+	})
+
+//go Button
+	document.getElementById('goButton').addEventListener('click',function(){
+		document.getElementById('goLight').style.background = 'green';
+	})
